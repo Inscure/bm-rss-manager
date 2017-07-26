d4d7cf646f120dc4840bca8a7f8c9b5b5a3dfeea
kujak00 <kujak00@acer-nitro>
2017-07-26 20:34:48 +0200

    Add method to set rss url.

--- src/Kujak00/BmRssManager/BmRssManager.php
+++ src/Kujak00/BmRssManager/BmRssManager.php
@@ -9,8 +9,26 @@ namespace Kujak00\BmRssManager;
  */
 class BmRssManager
 {
+	/**
+	 * RSS URL.
+	 * @var string
+	 */
+	private $rss_url = 'http://php.net/feed.atom';
+	
+	
+	
+	
 	public function __construct()
 	{
 		
+	}
+	
+	/**
+	 * 
+	 * @param string $url
+	 */
+	public function setRssUrl($url)
+	{
+		$this->rss_url = $url;
 	}
 }
