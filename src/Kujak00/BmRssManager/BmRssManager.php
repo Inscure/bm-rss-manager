7a7cde461493e33a46bf80285ec7f32c39fc3eb5
kujak00 <kujak00@acer-nitro>
2017-07-26 20:31:38 +0200

    Skeleton app.

---
+++ src/Kujak00/BmRssManager/BmRssManager.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kujak00\BmRssManager;
+
+/**
+ * RSS Manager.
+ *
+ * @author Paweł Kujaczyński <pawel@kujaczynski.pl>
+ */
+class BmRssManager
+{
+	public function __construct()
+	{
+		
+	}
+}
